64f014383929e5bd6f2adace3dc356d938be6706
mustafa0411 <125624594+mustafa0411@users.noreply.github.com>
2023-04-06 19:15:38 -0400

    Add files via upload

---
+++ Project 1.2/src/project1/test/Dog.java
@@ -0,0 +1,27 @@
+package project1.test;
+
+class Dog implements Comparable<Dog> {
+	public Integer age;
+
+	public Dog(int a) {
+		age = a;
+	}
+
+	@Override
+	public int compareTo(Dog o) {
+		return age.compareTo(o.age);
+	}
+
+	@Override
+	public String toString() {
+		return "Dog Age = " + age;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Dog))
+			return false;
+		else
+			return age == ((Dog) o).age;
+	}
+}
